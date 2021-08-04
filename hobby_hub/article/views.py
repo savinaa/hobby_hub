113dd27f62e9d6c80e660fb77b785c544e3b3e4a
savina <savin4eto_92@mail.bg>
2021-08-04 13:36:21 +0300

    corrected create_article so to not throw error for None returned

--- hobby_hub/article/views.py
+++ hobby_hub/article/views.py
@@ -18,5 +18,11 @@ def create_article(req):
     if form.is_valid():
         form.save()
         return redirect('index')
+    else:
+        form=ArticleForm()
+    context={
+        'form':form,
+    }
+    return render(req, 'article/create_article.html', context)
 
 
