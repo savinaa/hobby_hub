08fe611665b569b69b176dab00906d76405d00ea
savina <savin4eto_92@mail.bg>
2021-08-18 01:23:53 +0300

    created HobbyHubUser functionality

--- hobby_hub/hobby_hub_auth/models.py
+++ hobby_hub/hobby_hub_auth/models.py
@@ -2,29 +2,34 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+# from hobby_hub.hobby_hub_auth.managers import HobbyHubUserManager
+#
+#
 from hobby_hub.hobby_hub_auth.managers import HobbyHubUserManager
 
 
-class HobbyHubUser(AbstractBaseUser,PermissionsMixin):
-    email=models.EmailField(
+class HobbyHubUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
         unique=True,
     )
-    profile_picture = models.ImageField(
-        upload_to='profiles',
-        blank=True,
-    )
+    #    profile_picture = models.ImageField(
+    #        upload_to='profiles',
+    #        blank=True,
+    #    )
     is_staff = models.BooleanField(
         default=False,
     )
-    date_joined = models.DateTimeField(auto_now_add=True, )
-
-    USERNAME_FIELD='email'
-
-    #user = models.OneToOneField(
-    #    UserModel,
-    #    on_delete=models.CASCADE,
-    #    primary_key=True,
-    #)
+    #    date_joined = models.DateTimeField(auto_now_add=True, )
+    #
+    USERNAME_FIELD = 'email'
 #
+#    #user = models.OneToOneField(
+#    #    UserModel,
+#    #    on_delete=models.CASCADE,
+#    #    primary_key=True,
+#    #)
+##
     objects = HobbyHubUserManager()
+#
--- hobby_hub/hobby_hub_auth/urls.py
+++ hobby_hub/hobby_hub_auth/urls.py
@@ -1,9 +1,11 @@
+from django.conf.urls.static import static
 from django.urls import path
 
+from hobby_hub import settings
 from hobby_hub.hobby_hub_auth import views
 
 urlpatterns=[
     path('sign_up/', views.sign_up, name="sign up"),
     path('sign_in/', views.sign_in, name="sign in"),
     path('sign_out/', views.sign_out, name="sign out"),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
