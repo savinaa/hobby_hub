330c8924416eb9c2949dce63789030adba6c4ff4
savina <savin4eto_92@mail.bg>
2021-08-18 01:33:44 +0300

    added Users to the Admin panel

--- hobby_hub/hobby_hub_auth/admin.py
+++ hobby_hub/hobby_hub_auth/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from hobby_hub.hobby_hub_auth.models import HobbyHubUser
+
+
+@admin.register(HobbyHubUser)
+class HobbyHubAdmin(UserAdmin):
+    list_display = ('email', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'groups')
+    search_fields = ('email',)
+    ordering = ('email',)
